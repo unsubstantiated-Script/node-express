9a7b2dd3694ff1d9aa7e4529564b1240376ab602
Justin Fulton <jusfulton@gmail.com>
2020-08-03 19:21:45 -0700

    finalized with express router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const campsiteRouter = require('./routes/campsiteRouter');
 
 const hostname = "localhost";
 const port = 3000;
@@ -10,60 +11,17 @@ const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.all("/campsites", (req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next();
-});
+app.use('/campsites', campsiteRouter);
 
-app.get("/campsites", (req, res) => {
-  res.end("Will send all campsites to you");
-});
-
-app.post("/campsites", (req, res) => {
-  res.end(
-    `Will add campsite: ${req.body.name} with description: ${req.body.description}`
-  );
-});
-
-app.put("/campsites", (req, res) => {
-  res.statusCode = 403;
-  res.end("Put operation not supported on /campsites");
-});
-
-app.delete("/campsites", (req, res) => {
-  res.end("Deleting all campsites");
-});
-
-app.get("/campsites/:campsiteId", (req, res) => {
-  res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-});
-
-app.post("/campsites/:campsiteId", (req, res) => {
-  res.statusCode = 403;
-  res.end(
-    `POST operation not supported on /campsites/${req.params.campsiteId}`
-  );
-});
-
-app.put("/campsites/:campsiteId", (req, res) => {
-  res.write(`Updating the campsite: ${req.params.campsiteId}\n `);
-  res.end(`Will update the campsite: ${req.body.name}
-  with description: ${req.body.description}`);
-});
-
-app.delete("/campsites/:campsiteId", (req, res) => {
-  res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
 
 app.use(express.static(__dirname + "/public"));
 
 app.use((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
